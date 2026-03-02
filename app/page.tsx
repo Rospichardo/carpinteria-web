e716ae5d564bd61d2b5d79ab9fa5db43d6f689fd
Rosmery Pichardo <TU_CORREO_DE_GITHUB>
2026-03-02 11:59:38 -0600

    WhatsApp button con icono y animacion

--- app/page.tsx
+++ app/page.tsx
@@ -164,7 +164,7 @@ export default function Home() {
 
 {/* BOTON WHATSSAP */}
 <a
-  href="https://wa.me/5216241234567"
+  href="https://wa.me/5216242198270"
   target="_blank"
   rel="noopener noreferrer"
   className="whatsapp-button"
