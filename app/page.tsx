5214b879d59d398c875c295286ebd8f20b507bab
Rosmery Pichardo <TU_CORREO_DE_GITHUB>
2026-03-12 21:34:52 -0600

    imagenes miniaturas

--- app/page.tsx
+++ app/page.tsx
@@ -226,7 +226,8 @@ const [selectedImage, setSelectedImage] = useState<string | null>(null)
 {selectedProject !== null && (
   <div
     className="fixed inset-0 bg-black bg-opacity-90 
-               flex items-center justify-center z-50"
+               flex flex-col items-center justify-center 
+               z-50"
   >
     {/* cerrar */}
     <button
